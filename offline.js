0bb85c143de4165dea9c58f59daf1bd3da88dccd
cooltaby <cooltaby@users.noreply.github.com>
2021-02-10 12:57:59 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1612942116,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"media/btnclick.m4a",
+		"media/gunshoot.m4a",
+		"media/screem.m4a",
+		"media/screem.ogg",
+		"media/btnclick.ogg",
+		"media/gunshoot.ogg",
+		"media/bgmusic.m4a",
+		"media/bgmusic.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
